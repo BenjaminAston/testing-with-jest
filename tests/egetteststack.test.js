0fc2ed93164368f036d7fac8858326fb6696b0a0
BenjaminAston <aston.benjamin@gmail.com>
2025-05-09 16:41:30 +0200

    Fel

---
+++ tests/egetteststack.test.js
@@ -0,0 +1,8 @@
+const stack = require('../src/stack');
+
+test('double pop from single-element stack returns last value twice (FAIL)', () => {
+    stack.push('only');
+    stack.pop(); 
+    const result = stack.pop();
+    expect(result).toBe('only');
+});
